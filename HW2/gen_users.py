8c70e0804016cd475985f0ab7c8226bb5744af75
Adil <adil270120@gmail.com>
2025-02-02 21:52:39 +0500

    feat: change everything

--- HW2/gen_users.py
+++ HW2/gen_users.py
@@ -1,10 +1,15 @@
 from string import ascii_lowercase
 from random import randint, choices, sample
-import json, sys, psycopg2, os
+import json
+import sys
+import psycopg2
+import os
 from urllib.parse import urlparse
+
 
 def gen_users(n):
     scopes = ["push_send", "execute", "observe"]
+
     def gen_str(n):
         return ''.join(choices(ascii_lowercase, k=n))
 
@@ -14,6 +19,7 @@ def gen_users(n):
     users = [gen_user() for i in range(n)]
     with open("users.json", 'w') as f:
         f.write(json.dumps(users))
+
 
 def load_to_db():
     with open("users.json", 'r') as f:
@@ -25,7 +31,7 @@ def load_to_db():
         pass
     conn_url = os.getenv('DATABASE_URL')
     p = urlparse(conn_url)
-    pg_connection_dict = {a
+    pg_connection_dict = {
         'dbname': p.path[1:],
         'user': p.username,
         'password': p.password,
@@ -35,11 +41,13 @@ def load_to_db():
     try:
         conn = psycopg2.connect(**pg_connection_dict)
         with conn.cursor() as curs:
-            curs.executemany("insert into public.user(client_id, client_secret, scope) values (%s, %s, %s)", users)
+            curs.executemany(
+                "insert into public.user(client_id, client_secret, scope) values (%s, %s, %s)", users)
         conn.commit()
         conn.close()
     except:
         pass
+
 
 if "gen" in sys.argv:
     gen_users(1000)
--- HW2/locustfile.py
+++ HW2/locustfile.py
@@ -6,17 +6,18 @@ from json import JSONDecodeError
 with open("users.json", 'r') as f:
     users = json.loads(f.read())
 
+
 class WebsiteUser(FastHttpUser):
     @task
     def index(self):
         user = choice(users)
         scope = choice(user["scope"])
         with self.client.post("token/", data={
-                                                "client_id": user["client_id"],
-                                                "client_secret": user["client_secret"],
-                                                "scope": scope,
-                                                "grant_type": "client_credentials",
-                                            }) as resp:
+            "client_id": user["client_id"],
+            "client_secret": user["client_secret"],
+            "scope": scope,
+            "grant_type": "client_credentials",
+        }) as resp:
             try:
                 token = resp.json()["access_token"]
             except JSONDecodeError:
